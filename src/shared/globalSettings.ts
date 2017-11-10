6ffbdcf4a4828b8dfcacb8fd7d6120a591e489f6
dhanasekaranr <dhanasekaranr@hotmail.com>
2017-11-10 00:50:24 -0800

    cc

--- src/shared/globalSettings.ts
+++ src/shared/globalSettings.ts
@@ -4,9 +4,9 @@ import { Injectable } from "@angular/core";
 export class GlobalSettings {
 
  // private apiUrl = 'http://localhost:52146';
-    private apiUrl = 'http://localhost:52146';
+  //  private apiUrl = 'http://localhost:52146';
   //private apiUrl = 'http://localhost:50203';
-  //private apiUrl = 'http://www.api.icarebooks.com';
+  private apiUrl = 'http://www.api.icarebooks.com';
 
   constructor () {}
 
