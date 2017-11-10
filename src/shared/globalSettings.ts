1277b4269d5c2f3dd3ebcc6874b9d4e2680f29a8
dhanasekaranr <dhanasekaranr@hotmail.com>
2017-11-10 01:01:06 -0800

    commit today

--- src/shared/globalSettings.ts
+++ src/shared/globalSettings.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class GlobalSettings {
-
  // private apiUrl = 'http://localhost:52146';
   //  private apiUrl = 'http://localhost:52146';
   //private apiUrl = 'http://localhost:50203';
