99b0e647873bb0412b709bb117573e841505906a
dhanasekaranr <dhanasekaranr@hotmail.com>
2017-11-13 01:38:10 -0800

    hh

--- src/shared/service.ts
+++ src/shared/service.ts
@@ -12,8 +12,8 @@ export class icbService {
 
     constructor(private http: Http, private storage: Storage,private globalSettings: GlobalSettings) {
 
-      this.baseUrl = 'http://localhost:52146';//globalSettings.getSettings().apiUrl;
-      //this.baseUrl =   'http://www.api.icarebooks.com';
+     // this.baseUrl = 'http://localhost:52146';//globalSettings.getSettings().apiUrl;
+      this.baseUrl =   'http://www.api.icarebooks.com';
     }
 
 
