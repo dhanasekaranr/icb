db635d127f1777ba3c80a85d8d80c12c5997c84c
dhanasekaranr <dhanasekaranr@hotmail.com>
2018-09-15 21:01:13 -0700

    Version8.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
@@ -11,7 +11,6 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IonicStorageModule } from '@ionic/storage';
-<<<<<<< HEAD
 import { Authentication } from '../shared/authentication.service';
 import { CredentialsAuthentication } from '../shared/credentialsAuthentication';
 import { GlobalFunctions } from '../shared/globalFunctions';
@@ -20,7 +19,6 @@ import { ICBService } from '../shared/service';
 import { NotificationPopoverPage } from './home/notificationPopover';
 import { PopoverPage } from './home/popover';
 import { settingPopoverPage } from './home/settingPopover';
-
 @NgModule({
   declarations: [AppComponent, PopoverPage, NotificationPopoverPage, settingPopoverPage],
   entryComponents: [PopoverPage, NotificationPopoverPage, settingPopoverPage],
@@ -30,56 +28,9 @@ import { settingPopoverPage } from './home/settingPopover';
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ICBService, InAppBrowser,
-=======
-import { HttpModule } from '@angular/http';
-import { HttpClientModule} from "@angular/common/http";
-import { DashPage } from '../pages/dashboard/dash';
-import { DataServiceProvider } from '../providers/data-service/data-service';
-import { ScanPage } from '../pages/scanner/scan';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner';
-import { Toast } from '@ionic-native/toast';
-import { PopoverPage } from '../pages/home/popover';
-import { NotificationPopoverPage } from '../pages/home/notificationPopover';
-import { RentalsPage } from '../pages/home/rentals';
-import { StatsPage } from '../pages/stats/stats';
-
-import { InAppBrowser } from '@ionic-native/in-app-browser';
-import { TwitterConnect } from '@ionic-native/twitter-connect';
-import { TwitterService } from 'ng2-twitter';
-import { TwitterProvider } from '../providers/twitter/twitter';
-import { HistoryPage } from '../pages/history/history';
-@NgModule({
-  declarations: [
-    MyApp,
-    RegisterExternalUser,
-      Login,
-      Tab, HomePage, ContactPage, UserPage,BookInfo, CheckOutPage,ReturnPage, Search,MultiCopies,DashPage,ScanPage,
-      PopoverPage,NotificationPopoverPage,RentalsPage,StatsPage,HistoryPage
->>>>>>> Version 3.10
   ],
   bootstrap: [AppComponent],
   exports:      [],
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA,
-  ],
-<<<<<<< HEAD
-=======
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    RegisterExternalUser,
-    Login,
-      Tab, HomePage, ContactPage, UserPage, BookInfo, CheckOutPage,ReturnPage,MultiCopies,DashPage,ScanPage,PopoverPage,
-      NotificationPopoverPage,RentalsPage,StatsPage,HistoryPage
-  ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler},
-    DataServiceProvider, BarcodeScanner, TwitterConnect,
-    TwitterService,
-    InAppBrowser,
-    TwitterProvider,
-    Toast,
-    TwitterProvider]
->>>>>>> Version 3.10
 })
 export class AppModule {
 
--- src/shared/globalSettings.ts
+++ src/shared/globalSettings.ts
@@ -2,13 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class GlobalSettings {
-<<<<<<< HEAD
   //   private apiUrl = 'http://localhost:52146';
     private apiUrl = 'http://api.icarebooks.com';
-=======
-    private apiUrl = 'http://localhost:52146';
-  //private apiUrl = 'http://api.icarebooks.com';
->>>>>>> Version 3.10
 
   constructor() {}
 
