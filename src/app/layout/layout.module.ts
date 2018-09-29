95b5c9bb8d9d49ed40fac6f255d86afc7bee3f1f
dhanasekaranr <dhanasekaranr@hotmail.com>
2018-09-29 14:07:44 -0700

    8.1.4

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,13 +1,14 @@
-
-import { CommonModule } from '@angular/common';
+import { HeaderPage } from './header';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [IonicModule,
     CommonModule,
     FormsModule],
-
+  declarations: [HeaderPage],
+  exports: [HeaderPage]
 })
 export class LayoutModule { }
